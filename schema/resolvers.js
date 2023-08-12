c86d6a956a4e6f5d6cad573e2f04a0a7ba96759e
nickguerra <nickg@maliciousgames.com>
2023-08-12 05:30:57 -0500

    Added mutations

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -24,6 +24,41 @@ const resolvers = {
             const movie = _.find(MovieList, { name });
             return movie;
         }
+    },
+
+    User: {
+        favoriteMovies: () => {
+            return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010)
+        }
+    },
+
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input;
+            const lastId = UserList[UserList.length - 1].id;
+            user.id = lastId + 1;
+            UserList.push(user);
+            return user;
+        },
+
+        updateUsername: (parent, args) => {
+            const { id, newUsername } = args.input;
+            let userUpdated;
+            UserList.forEach((user) => {
+                if (user.id === id) {
+                    user.username = newUsername;
+                    userUpdated = user
+                }
+            });
+
+            return userUpdated;
+        },
+
+        deleteUser: (parent, args) => {
+            const id = args.id;
+            _.remove(UserList, (user) => user.id === Number(id));
+            return null;
+        }
     }
 };
 
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -8,6 +8,7 @@ const typeDefs = gql`
         age: Int!
         nationality: Nationality!
         friends: [User]
+        favoriteMovies: [Movie]
     }
 
     type Movie {
@@ -22,6 +23,25 @@ const typeDefs = gql`
         user(id: ID!): User!
         movies: [Movie!]!
         movie(name: String!): Movie!
+    }
+
+    input CreateUserInput {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = BRAZIL
+    }
+
+    input UpdateUsernameInput {
+      id: ID!
+      newUsername: String!
+    }
+
+
+    type Mutation {
+        createUser(input: CreateUserInput!): User
+        updateUsername(input: UpdateUsernameInput!): User
+        deleteUser(id: ID!): User
     }
 
     enum Nationality {
